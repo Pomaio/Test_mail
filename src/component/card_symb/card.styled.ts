651294fefb57f8cd3fa6189accf31dd625b77ed6
grish <grisha_marshinov@mail.ru>
2019-07-18 11:26:57 +0300

    optimize style code

--- src/component/card_symb/card.styled.ts
+++ src/component/card_symb/card.styled.ts
@@ -1,32 +1,36 @@
 import styled from 'styled-components';
 
-export const Box = styled.span`
+
+const box_props = `
     display: block;
     width: 25px;
     height: 32px;
     border-radius: 2px;
-
     margin-left: 2px;
     margin-right: 2px;
-    background: #F0F0F0;
 `;
-export const BoxI = styled.input`
+
+const border_input_props = `    
     text-align: center;
     font-family: ArialMT;
     font-size: 15px;
-
-    display: block;
-    width: 25px;
-    height: 32px;
-    border-radius: 2px;
-    margin-left: 2px;
-    margin-right: 2px;
-
     border: 1px solid rgba(0, 0, 0, 0.12);
     box-sizing: border-box;
     padding: 0;
     background: #FFFFFF;
     outline:none;
+`;
+
+export const Box = styled.span`
+   
+    ${box_props}
+
+    background: #F0F0F0;
+`;
+export const BoxI = styled.input`
+
+    ${box_props}
+    ${border_input_props}
 
     &:hover{ 
         border: 1px solid rgba(0, 0, 0, 0.24);
